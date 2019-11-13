63d2f43539af443dbcc63bae403dc9258c5fe65b
raccoonsdream <j.rochna.official@gmail.com>
2019-11-13 21:29:56 +0100

    hubcommit

---
+++ QuizApp/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+namespace QuizApp
+{
+    public class GameHub : Hub
+    {
+        public void SendQuestion(string question)
+        {
+            //Clients.All.showQuestion(question);
+        }
+    }
+}
